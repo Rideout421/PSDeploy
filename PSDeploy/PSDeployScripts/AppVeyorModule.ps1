f079bde40a470088b2b405d56d13e70bdeaa2c27
Mark Kraus <github@markekraus.com>
2018-01-08 15:14:48 -0600

    Skip NuGet PackageProvider install on PSCore

--- PSDeploy/PSDeployScripts/AppVeyorModule.ps1
+++ PSDeploy/PSDeployScripts/AppVeyorModule.ps1
@@ -153,7 +153,11 @@ function New-Nuspec
     Set-Content -Path $nuspecPath -Value $content
 } # New-Nuspec
 
-$null = Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+if ('Core' -ne $PSVersionTable.PSEdition)
+{
+    $null = Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+}
+
 
 foreach($Deploy in $Deployment) {
 
